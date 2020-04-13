a82aac7cf698328f62f091a0df6920eb0862c9ad
Seigneurhol <seigneurholo@hotmail.fr>
2020-04-13 19:18:18 +0200

    Tweak the flower animation to be closer to the Apple one

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -3,59 +3,82 @@ import { createUseStyles } from "react-jss";
 
 const useStyles = createUseStyles({
   "@keyframes bounceCircle1": {
-    from: {
-      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)"
+    "0%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    to: {
-      transform: "rotate(90deg) translate(-35px, -50px) scale(1)"
-    }
+    "40%, 45%": {
+      transform: "rotate(90deg) translate(-35px, -50px) scale(1)",
+    },
+    "95%, 100%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
+    },
   },
   "@keyframes bounceCircle2": {
-    from: {
-      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)"
+    "0%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    to: {
-      transform: "rotate(90deg) translate(35px, -50px) scale(1)"
-    }
+    "40%, 45%": {
+      transform: "rotate(90deg) translate(35px, -50px) scale(1)",
+    },
+    "95%, 100%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
+    },
   },
   "@keyframes bounceCircle3": {
-    from: {
-      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)"
+    "0%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    to: {
-      transform: "rotate(90deg) translate(-60px, 0) scale(1)"
-    }
+    "40%, 45%": {
+      transform: "rotate(90deg) translate(-60px, 0) scale(1)",
+    },
+    "95%, 100%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
+    },
   },
   "@keyframes bounceCircle4": {
-    from: {
-      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)"
+    "0%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    to: {
-      transform: "rotate(90deg) translate(60px, 0) scale(1)"
-    }
+    "40%, 45%": {
+      transform: "rotate(90deg) translate(60px, 0) scale(1)",
+    },
+    "95%, 100%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
+    },
   },
   "@keyframes bounceCircle5": {
-    from: {
-      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)"
+    "0%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    to: {
-      transform: "rotate(90deg) translate(-35px, 50px) scale(1)"
-    }
+    "40%, 45%": {
+      transform: "rotate(90deg) translate(-35px, 50px) scale(1)",
+    },
+    "95%, 100%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
+    },
   },
   "@keyframes bounceCircle6": {
-    from: {
-      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)"
+    "0%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    to: {
-      transform: "rotate(90deg) translate(35px, 50px) scale(1)"
-    }
+    "40%, 45%": {
+      transform: "rotate(90deg) translate(35px, 50px) scale(1)",
+    },
+    "95%, 100%": {
+      transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
+    },
   },
   app: {
     display: "flex",
+    padding: "100px 200px",
+    minHeight: "100vh",
+  },
+  flower: {
+    display: "flex",
     justifyContent: "center",
     alignItems: "center",
-    minHeight: "100vh",
-    transform: "rotate(90deg)"
+    transform: "rotate(90deg)",
+    height: "250px",
   },
   circle: {
     width: "125px",
@@ -65,47 +88,57 @@ const useStyles = createUseStyles({
     mixBlendMode: "screen",
     "&:nth-child(1)": {
       animation:
-        "4.2857s cubic-bezier(0.455, 0.030, 0.515, 0.955) $bounceCircle1 infinite alternate"
+        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle1 infinite",
     },
     "&:nth-child(2)": {
       animation:
-        "4.2857s cubic-bezier(0.455, 0.030, 0.515, 0.955) $bounceCircle2 infinite alternate"
+        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle2 infinite",
     },
     "&:nth-child(3)": {
       animation:
-        "4.2857s cubic-bezier(0.455, 0.030, 0.515, 0.955) $bounceCircle3 infinite alternate"
+        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle3 infinite",
     },
     "&:nth-child(4)": {
       animation:
-        "4.2857s cubic-bezier(0.455, 0.030, 0.515, 0.955) $bounceCircle4 infinite alternate"
+        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle4 infinite",
     },
     "&:nth-child(5)": {
       animation:
-        "4.2857s cubic-bezier(0.455, 0.030, 0.515, 0.955) $bounceCircle5 infinite alternate"
+        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle5 infinite",
     },
     "&:nth-child(6)": {
       animation:
-        "4.2857s cubic-bezier(0.455, 0.030, 0.515, 0.955) $bounceCircle6 infinite alternate"
+        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle6 infinite",
     },
     "&:nth-child(odd)": {
-      background: "#61bea2"
+      background: "#61bea2",
     },
     "&:nth-child(even)": {
-      background: "#529ca0"
-    }
-  }
+      background: "#529ca0",
+    },
+  },
+  choice: {
+    color: "white",
+  },
 });
 
 function App() {
   const classes = useStyles();
   return (
     <div className={classes.app}>
+      <div className={classes.flower}>
         <div className={classes.circle}></div>
         <div className={classes.circle}></div>
         <div className={classes.circle}></div>
         <div className={classes.circle}></div>
         <div className={classes.circle}></div>
         <div className={classes.circle}></div>
+      </div>
+      <div className={classes.choice}>
+        <p>7 breath</p>
+        <input value="1 min" />
+        <button onClick={() => console.log("salut")}>Start</button>
+      </div>
     </div>
   );
 }
