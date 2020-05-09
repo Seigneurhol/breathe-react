e8a47823983d6945d3e32bb318ba9b571372cbb6
Seigneurhol <seigneurholo@hotmail.fr>
2020-05-09 21:53:16 +0200

    Small timing modification to be closer to the watch animation. Won't touch it again. Perfect for me ;)

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,7 +6,7 @@ const useStyles = createUseStyles({
     "0%": {
       transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    "40%, 45%": {
+    "37.5%, 42.5%": {
       transform: "rotate(90deg) translate(-35px, -50px) scale(1)",
     },
     "95%, 100%": {
@@ -17,7 +17,7 @@ const useStyles = createUseStyles({
     "0%": {
       transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    "40%, 45%": {
+    "37.5%, 42.5%": {
       transform: "rotate(90deg) translate(35px, -50px) scale(1)",
     },
     "95%, 100%": {
@@ -28,7 +28,7 @@ const useStyles = createUseStyles({
     "0%": {
       transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    "40%, 45%": {
+    "37.5%, 42.5%": {
       transform: "rotate(90deg) translate(-60px, 0) scale(1)",
     },
     "95%, 100%": {
@@ -39,7 +39,7 @@ const useStyles = createUseStyles({
     "0%": {
       transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    "40%, 45%": {
+    "37.5%, 42.5%": {
       transform: "rotate(90deg) translate(60px, 0) scale(1)",
     },
     "95%, 100%": {
@@ -50,7 +50,7 @@ const useStyles = createUseStyles({
     "0%": {
       transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    "40%, 45%": {
+    "37.5%, 42.5%": {
       transform: "rotate(90deg) translate(-35px, 50px) scale(1)",
     },
     "95%, 100%": {
@@ -61,7 +61,7 @@ const useStyles = createUseStyles({
     "0%": {
       transform: "rotate(0deg) translate(0px, 0px) scale(0.2)",
     },
-    "40%, 45%": {
+    "37.5%, 42.5%": {
       transform: "rotate(90deg) translate(35px, 50px) scale(1)",
     },
     "95%, 100%": {
@@ -88,27 +88,27 @@ const useStyles = createUseStyles({
     mixBlendMode: "screen",
     "&:nth-child(1)": {
       animation:
-        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle1 infinite",
+        "8.5714285714s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle1 infinite",
     },
     "&:nth-child(2)": {
       animation:
-        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle2 infinite",
+        "8.5714285714s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle2 infinite",
     },
     "&:nth-child(3)": {
       animation:
-        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle3 infinite",
+        "8.5714285714s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle3 infinite",
     },
     "&:nth-child(4)": {
       animation:
-        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle4 infinite",
+        "8.5714285714s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle4 infinite",
     },
     "&:nth-child(5)": {
       animation:
-        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle5 infinite",
+        "8.5714285714s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle5 infinite",
     },
     "&:nth-child(6)": {
       animation:
-        "8.5s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle6 infinite",
+        "8.5714285714s cubic-bezier(0.420, 0.000, 0.580, 1.000) $bounceCircle6 infinite",
     },
     "&:nth-child(odd)": {
       background: "#61bea2",
